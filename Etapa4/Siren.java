eeb1664cd56bf62f8059bdcab31c43856777701f
Nxisn <106633606+Nxisn1@users.noreply.github.com>
2023-04-19 23:10:10 -0400

    javadoc

--- Etapa4/Siren.java
+++ Etapa4/Siren.java
@@ -5,6 +5,11 @@ public class Siren {
     private URL dir;
     private boolean isSounding;
     private AePlayWave aWave;
+    
+   /*
+   * Obtener y guardar la sirena.
+   * @param soundFileName Es el nombre del archivo de sonido para la alarma con extensión wav
+   */
     public Siren (String soundFileName){
         try {
             dir = new File(soundFileName).toURI().toURL();
@@ -14,6 +19,10 @@ public class Siren {
         }
         isSounding = false;
     }
+    
+    /*
+    * Pone a sonar la alarma.
+    */
     public void play(){
         isSounding = true;
         aWave = new AePlayWave(dir);
@@ -26,15 +35,27 @@ public class Siren {
             System.out.println("Ha ocurrido un problema con el thread");
         }
     }
+    
+    /*
+    * Detiene el sonido de la alarma.
+    */
     public void stop(){
         if (isSounding){
             isSounding = false;
             aWave.stopSounding();
         }
     }
+    
+    /*
+    * Obtiene el header de la sirena.
+    */
     public String getHeader() {
         return "Siren";
     }
+    
+    /*
+    * Obtiene el estado de la sirena.
+    */
     public int getState() {
         if (isSounding)
             return 1;
