9126b2097d4145f80b274351b345d59bd4da14d8
Nxisn <106633606+Nxisn1@users.noreply.github.com>
2023-04-19 23:13:57 -0400

    javadoc

--- Etapa4/Sensor.java
+++ Etapa4/Sensor.java
@@ -1,20 +1,42 @@
 public class Sensor {
     private SwitchState state;
+    
+    /*
+    * Setea el estado del sensor a abierto.
+    */
     public Sensor(){
         this(SwitchState.OPEN);
     }
+    
+    /*
+    * Cambia el estado la sirena.
+    */
     public Sensor(SwitchState s){
         state = s;
     }
+    
+    /*
+    * Obtiene el estado de la sirena.
+    * @return 1 si está abierta la sirena y 0 en caso contrario
+    */
     public int getState(){
         if (state == SwitchState.OPEN)
             return 1;
         else
             return 0;
     }
+    
+    /*
+    * Cambia el estado la sirena.
+    */
     protected void setState(SwitchState s) {
         state = s;
     }
+    
+    /*
+    * Obtiene el estado de la sirena.
+    * @return 1 si está cerrada la sirena y 0 en caso contrario
+    */
     public String toString(){
         if (state== SwitchState.CLOSE)
             return "1";
